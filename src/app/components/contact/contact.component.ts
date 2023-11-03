f7a3275d2586c586b80290272386d77d3bb6e8be
ihimanshuparihar <himanshuparihar1101@gmail.com>
2023-11-04 03:12:02 +0530

    next btn added

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-contact',
-  templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.css']
+  selector: "app-contact",
+  templateUrl: "./contact.component.html",
+  styleUrls: ["./contact.component.css"],
 })
 export class ContactComponent {
-
+  name: any;
+  email: any;
+  phoneNumber: any;
+  queryTopic: any;
+  queryDescription: any;
 }
